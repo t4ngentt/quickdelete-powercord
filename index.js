be37022222aa699e3a1f17f624b278612c48c4e7
nikhil <rohanshegokar@gmail.com>
2021-05-02 22:04:29 +0530

    final changes, its completed ig
    yeah

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Plugin } = require('powercord/entities');
 const { findInReactTree } = require('powercord/util');
 const { inject, uninject } = require('powercord/injector');
 const { React, getModule } = require('powercord/webpack');
+const http = require('powercord/http');
 
 module.exports = class quickdelete extends Plugin {
 
@@ -11,33 +12,26 @@ module.exports = class quickdelete extends Plugin {
       ...await getModule([ 'button', 'separator', 'wrapper' ])
     };
 
-    const MiniPopover = await getModule(m => m.default && m.default.displayName === 'MiniPopover');
+    const pog = await getModule(m => m.default && m.default.displayName === 'pog');
 
-    inject('quickdelete', MiniPopover, 'default', (_, res) => {
-      const props = findInReactTree(res, r => r && r.message);
-      
+    inject('quickdelete', pog, 'default', (_, res) => {
+      const props = findInReactTree(res, r => r && r.message);      
       var messageid = props.message.id;
       var channelid = props.message.channel_id;
-
       res.props.children.unshift(React.createElement(
         'div', {
           className: classes.button,
-          onClick: () =>{
-            const API_DELETE_URL = `https://discord.com/api/v9/channels/${channelid}/messages/${messageid}`;
-            await fetch(API_DELETE_URL, {
-                        headers,
-                        method: 'DELETE'
-            });
+          onClick: () => {
+            console.log("it clicked");
+            require('powercord/webpack').messages.deleteMessage(channelid, messageid)
           }
         },  
-        React.createElement('img', {
-          className: `emoji ${classes.icon}`,
-          src: '/assets/e4d52f4d69d7bba67e5fd70ffe26b70d.svg'
+        React.createElement('img', {className: `emoji ${classes.icon}`,src: '/assets/9b1b76abb59b5c8a41daea1342f2138a.svg'
         })
       ));
       return res;
     });
-    MiniPopover.default.displayName = 'MiniPopover';
+    pog.default.displayName = 'pog';
   }
 
   pluginWillUnload () {
